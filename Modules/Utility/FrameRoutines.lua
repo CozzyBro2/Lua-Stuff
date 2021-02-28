908b124c72824598359b54ea3b139da11fd7d8be
trollcat <76234965+CozzyBro2@users.noreply.github.com>
2021-02-28 13:40:45 -0500

    Update FrameRoutines.lua

--- Modules/Utility/FrameRoutines.lua
+++ Modules/Utility/FrameRoutines.lua
@@ -1,9 +1,9 @@
 local frameRoutines = {RenderStepped = game.Players.LocalPlayer and {}, Heartbeat = {}}
 
 for routineType, routines in pairs(frameRoutines) do
-    game:GetService("RunService")[routineType]:Connect(function(deltaTime)
+    game:GetService("RunService")[routineType]:Connect(function(...)
         for _, routine in ipairs(routines) do
-            routine(deltaTime)
+            routine(...)
         end
     end)
     
