dfb81c8395afa44886bd9a8c0bbe447cb864aff0
trollcat <76234965+CozzyBro2@users.noreply.github.com>
2021-03-05 10:47:26 -0500

    made lot better

--- Modules/Utility/Input.lua
+++ Modules/Utility/Input.lua
@@ -1,35 +1,19 @@
---[[ TODO:
-     Stop returning a function, remove the do end, remove the dumb variables, etc.
---]]
+local boundInputs = {}
 
-local boundInputs, bindList = {}, {} do
-	local HandleInput = function(input, isFocused)
-		local foundInput = boundInputs[input.KeyCode == Enum.KeyCode.Unknown and input.UserInputType or input.KeyCode]
+local HandleInput = function(input, isFocused)
+	local foundInput = boundInputs[input.KeyCode == Enum.KeyCode.Unknown and input.UserInputType or input.KeyCode]
 
-		if foundInput then
-			for _, callback in ipairs(foundInput) do
-				callback(input, isFocused)
-			end
+	if foundInput then
+		for _, callback in ipairs(foundInput) do
+			callback(input, isFocused)
 		end
 	end
-	
-	local inputService = game:GetService("UserInputService")
-	
-	inputService.InputChanged:Connect(HandleInput)
-	inputService.InputBegan:Connect(HandleInput)
-	inputService.InputEnded:Connect(HandleInput)
 end
+	
+local inputService = game:GetService("UserInputService")
+	
+inputService.InputChanged:Connect(HandleInput)
+inputService.InputBegan:Connect(HandleInput)
+inputService.InputEnded:Connect(HandleInput)
 
-return function(callback, inputs)
-	if not bindList[callback] then bindList[callback] = inputs end
-	
-	local inputList = inputs or bindList[callback]
-	
-	for _, input in ipairs(inputList) do
-		if inputs and boundInputs[input] then
-			table.insert(boundInputs[input], callback); return
-		end
-		
-		boundInputs[input] = inputs and {callback}
-	end
-end
+return boundInputs
