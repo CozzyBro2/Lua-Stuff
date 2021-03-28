03c8b00f6c94117ddf48a2563b84d22cbfcafe65
trollcat <76234965+CozzyBro2@users.noreply.github.com>
2021-03-28 00:18:36 -0400

    Removed double indexing for input.KeyCode + cool naming
    Made the variable names better in some places, and removed double indexing for input.KeyCode in the event that it was a keycode input used

--- Modules/Utility/Input.lua
+++ Modules/Utility/Input.lua
@@ -1,19 +1,20 @@
-local boundInputs = {}
+local inputBinds = {}
 
-local HandleInput = function(input, isFocused)
-	local foundInput = boundInputs[input.KeyCode == Enum.KeyCode.Unknown and input.UserInputType or input.KeyCode]
+local HandleInput = function(inputObject) 
+    local keyCode = inputObject.KeyCode
+    local boundInput = inputBinds[keyCode == Enum.KeyCode.Unknown and inputObject.UserInputType or keyCode]
 
-	if foundInput then
-		for _, callback in ipairs(foundInput) do
-			callback(input, isFocused)
-		end
-	end
+    if boundInput then
+        for _, callback in ipairs(boundInput.Callbacks) do
+            callback(inputObject)
+        end
+    end
 end
-	
+
 local inputService = game:GetService("UserInputService")
-	
+
 inputService.InputChanged:Connect(HandleInput)
 inputService.InputBegan:Connect(HandleInput)
 inputService.InputEnded:Connect(HandleInput)
 
-return boundInputs
+return inputBinds
