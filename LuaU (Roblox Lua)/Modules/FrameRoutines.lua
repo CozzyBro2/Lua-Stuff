6473747fde62a983d093096e7815bb0309d15b70
trollcat <76234965+CozzyBro2@users.noreply.github.com>
2021-04-03 13:10:50 -0400

    clarified some things, generalized it some

--- LuaU (Roblox Lua)/Modules/FrameRoutines.lua
+++ LuaU (Roblox Lua)/Modules/FrameRoutines.lua
@@ -1,7 +1,7 @@
-local frameRoutines = {PreSimulation = {}}
+local frameRoutines = {PreSimulation = game.Players.LocalPlayer and {}} -- Add support for more events here as you see fit 
 
 for routineType, routines in pairs(frameRoutines) do
-    game:GetService("RunService")[routineType]:Connect(function(...)
+    game:GetService("RunService")[routineType]:Connect(function(...) -- swap for a single named arg if you don't use Stepped or it's equivalents
         for _, routine in ipairs(routines) do
             routine(...)
         end
