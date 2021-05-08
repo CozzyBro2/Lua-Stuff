7cc95430c4176e65adb56326d39114bcc756d767
GavinW <76234965+CozzyBro2@users.noreply.github.com>
2021-05-08 11:50:55 -0400

    i hate bad spacing

--- LuaU (Roblox Lua)/Modules/Scheduler.lua
+++ LuaU (Roblox Lua)/Modules/Scheduler.lua
@@ -2,25 +2,25 @@ local CallbackMap = {} -- { Callback = CallbackInfo }
 local Scheduled = {} -- { CallbackInfo = os.clock }
 
 game:GetService("RunService").Stepped:Connect(function()
-	for CallbackInfo, CallDate in pairs(Scheduled) do
-		if os.clock() - CallDate < 0 then return end
-		
-		local CallArgs = CallbackInfo.Args
-		
-		CallbackInfo.Call( CallArgs and unpack(CallArgs) )
-	end
+    for CallbackInfo, CallDate in pairs(Scheduled) do
+        if os.clock() - CallDate < 0 then return end
+        
+        local CallArgs = CallbackInfo.Args
+        
+        CallbackInfo.Call(CallArgs and unpack(CallArgs))
+    end
 end)
 
 return {
-	Insert = function(Callback, Time, ...)
-		local CallbackInfo = {Call = Callback, Args = ( ... and {...} )}
-		
-		Scheduled[CallbackInfo] = (os.clock() + Time) - 0.005
-		
-		CallbackMap[Callback] = CallbackInfo
-	end,
-	
-	Remove = function(Callback)
-		Scheduled[CallbackMap[Callback]] = nil
-	end
+    Insert = function(Callback, Time, ...)
+        local CallbackInfo = {Call = Callback, Args = ( ... and {...} )}
+        
+        Scheduled[CallbackInfo] = (os.clock() + Time) - 0.005
+        
+        CallbackMap[Callback] = CallbackInfo
+    end,
+    
+    Remove = function(Callback)
+        Scheduled[CallbackMap[Callback]] = nil
+    end
 }
