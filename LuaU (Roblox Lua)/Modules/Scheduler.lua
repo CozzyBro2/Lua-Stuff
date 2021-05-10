a9e98922360244c80391d00949e9d22dae203563
GavinW <76234965+CozzyBro2@users.noreply.github.com>
2021-05-09 21:48:45 -0400

    last minute fix

--- LuaU (Roblox Lua)/Modules/Scheduler.lua
+++ LuaU (Roblox Lua)/Modules/Scheduler.lua
@@ -3,28 +3,28 @@ local Scheduled = {} -- { CallbackInfo = os.clock }
 local ScheduleFuncs = {}
 
 game:GetService("RunService").Stepped:Connect(function()
-    for CallbackInfo, CallDate in pairs(Scheduled) do
-        if os.clock() > CallDate then return end
-        
-        local Call = CallbackInfo.Call
-        ScheduleFuncs.Remove(Call)
-            
-        Call(unpack(CallbackInfo.Args))
-    end
+	for CallbackInfo, CallDate in pairs(Scheduled) do
+		if os.clock() < CallDate then return end
+
+		local Call = CallbackInfo.Call
+		ScheduleFuncs.Remove(Call)
+
+		Call(unpack(CallbackInfo.Args))
+	end
 end)
 
 function ScheduleFuncs.Schedule(Time, Callback, ...)
-    local CallbackInfo = {Call = Callback, Args = {...}}
+	local CallbackInfo = {Call = Callback, Args = {...}}
 
-    Scheduled[CallbackInfo] = (os.clock() + Time) - 0.005
+	Scheduled[CallbackInfo] = (os.clock() + Time) - 0.005
 
-    CallbackMap[Callback] = CallbackInfo
+	CallbackMap[Callback] = CallbackInfo
 end
 
 function ScheduleFuncs.Remove(Callback)
-    Scheduled[CallbackMap[Callback]] = nil
+	Scheduled[CallbackMap[Callback]] = nil
 
-    CallbackMap[Callback] = nil
+	CallbackMap[Callback] = nil
 end
 
 return ScheduleFuncs
