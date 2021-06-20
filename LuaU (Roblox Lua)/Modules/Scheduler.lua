3eefdeb2b0b9296350b8fce8a70b8c3b1d72b0b1
GavinW <76234965+CozzyBro2@users.noreply.github.com>
2021-06-19 22:29:57 -0400

    Readability
    I tried to make it more readable, please pull request if you've got a better idea to make it moreso.

--- LuaU (Roblox Lua)/Modules/Scheduler.lua
+++ LuaU (Roblox Lua)/Modules/Scheduler.lua
@@ -1,30 +1,33 @@
 local CallbackMap = {} -- { Callback = CallbackInfo }
 local Scheduled = {} -- { CallbackInfo = os.clock }
-local ScheduleFuncs = {}
+local Scheduler = {}
 
 game:GetService("RunService").Heartbeat:Connect(function()
 	for CallbackInfo, CallDate in pairs(Scheduled) do
 		if os.clock() < CallDate then continue end
 
 		local Call = CallbackInfo.Call
-		ScheduleFuncs.Remove(Call)
-		
+		Scheduler.Remove(Call)
+
 		Call(unpack(CallbackInfo.Args))
 	end
 end)
 
-function ScheduleFuncs.Schedule(Time, Callback, ...)
-	local CallbackInfo = {Call = Callback, Args = {...}}
+function Scheduler.Add(Time, Callback, ...)
+	local CallbackInfo = {
+		Call = Callback, 
+		Args = {...}
+	}
 
 	Scheduled[CallbackInfo] = (os.clock() + Time) - 0.005
-
 	CallbackMap[Callback] = CallbackInfo
 end
 
-function ScheduleFuncs.Remove(Callback)
-	Scheduled[CallbackMap[Callback]] = nil
+function Scheduler.Remove(Callback)
+	local ToRemove = CallbackMap[Callback]
 
+	Scheduled[ToRemove] = nil
 	CallbackMap[Callback] = nil
 end
 
-return ScheduleFuncs
+return Scheduler
