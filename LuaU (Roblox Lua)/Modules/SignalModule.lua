571a3ec41b9bfb17ff69574a1382479d67592a44
GavinW <76234965+CozzyBro2@users.noreply.github.com>
2021-07-14 19:35:57 -0400

    Cool changes

--- LuaU (Roblox Lua)/Modules/SignalModule.lua
+++ LuaU (Roblox Lua)/Modules/SignalModule.lua
@@ -2,61 +2,61 @@ local Service = {Subscriptions = {}}
 Service.__index = Service
 
 function Service.new(Parameters)
-    assert(Parameters, "Invalid parameters")
-    assert(Parameters.Topic, "Invalid topic")
+	assert(Parameters, "Null parameters")
+	assert(Parameters.Topic, "Null topic")
 
-    local Subscription = setmetatable({
+	local Subscription = setmetatable({
 
-        _WrapCalls    = Parameters.WrapCalls,
-        _ResumePoints = {},
-        _CanListen    = true,
-        Topic         = Parameters.Topic,
+		_WrapCalls 		= Parameters.WrapCalls,
+		_ResumePoints   = {},
+		_CanListen 		= true,
+		Topic 			= Parameters.Topic,
 
-    }, Service)
+	}, Service)
 
-    Service.Subscriptions[Subscription.Topic] = Subscription
-    
-    return Subscription
+	Service.Subscriptions[Subscription.Topic] = Subscription
+	
+	return Subscription
 end
 
 function Service:Connect(Callback)
-    assert(Callback and type(Callback) == "function", "Expected type 'function'")
-
-    Callback = self._WrapCalls and coroutine.wrap(Callback) or Callback    
-
-    self._ResumePoints[Callback] = {}
+	assert(type(Callback) == "function", "Expected type 'function'")
+	
+	Callback = self._WrapCalls and coroutine.wrap(Callback) or Callback
+	
+	self._ResumePoints[Callback] = {}
 end
 
 function Service:Wait()
-    self._ResumePoints[coroutine.running()] = {}
+	self._ResumePoints[coroutine.running()] = {}
 
-    return coroutine.yield()
+	return coroutine.yield()
 end
-    
+	
 function Service:Post(...)
-    local ResumePoints = self._ResumePoints
+	local ResumePoints = self._ResumePoints
 
-    for Point in pairs(ResumePoints) do
-        local Resumed = typeof(Point) == "function" and Service._Call(Point, ...) or Service._Resume(Point, ...)
+	for Point in pairs(ResumePoints) do
+		local Resumed = typeof(Point) == "function" and Service._Call(Point, ...) or Service._Resume(Point, ...)
 
-        ResumePoints[Point] = nil
-    end
+		ResumePoints[Point] = nil
+	end
 end
 
-function Service:Revoke(Topic)
-    self._CanListen = false
+function Service:Revoke()
+	self._CanListen = false
 
-    Service.Subscriptions[Topic] = nil
+	Service.Subscriptions[self.Topic] = nil
 end
 
 function Service:_Resume(Thread, ...)
-    coroutine.resume(Thread, ...)
+	coroutine.resume(Thread, ...)
 end
 
 function Service:_Call(Callback, ...)
-    Callback(...)
+	Callback(...)
 
-    return Callback
+	return Callback
 end
 
 return Service
