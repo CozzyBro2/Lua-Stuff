8fb32e2b3f6ef7f33f7eb780d7ae0d59f26c7f29
GavinW <76234965+CozzyBro2@users.noreply.github.com>
2021-07-15 12:40:27 -0400

    Revamp in progress
    changed like everything

--- LuaU (Roblox Lua)/Modules/Game Modules/SoundsModule/init.lua
+++ LuaU (Roblox Lua)/Modules/Game Modules/SoundsModule/init.lua
@@ -1,34 +1,83 @@
 --[[
-	Name: SoundsModule.lua
+	Name: FootstepController.lua
 --]]
 
-local Sounds = require(script.Sounds) --// {Enum.Materials.SomeMaterial = "SomeSound"}
+local Module = {}
+Module.__index = Module
 
-return {
-	Start = function(Character)
-		local Humanoid = Character:WaitForChild("Humanoid")
-		local SoundPlayer = Character:WaitForChild("LowerTorso")
-		local Sound = SoundPlayer.Concrete
+local StepMap = require(script.StepMap)
 
-		Humanoid:GetPropertyChangedSignal("FloorMaterial"):Connect(function()
-			local MappedMaterial = Sounds[Humanoid.FloorMaterial] 
-			local NewSound = MappedMaterial and SoundPlayer:FindFirstChild(MappedMaterial)
+function Module.new(Character)
+	local self do
+		local Humanoid = assert(Character:WaitForChild("Humanoid", 2), "Null humanoid")
+		local Player   = assert(Character:WaitForChild("LowerTorso", 2):FindFirstChild("Sounds"), "Null sound player")
 
-			if NewSound then
-				NewSound.TimePosition = Sound.TimePosition
-				NewSound.PlaybackSpeed = Sound.PlaybackSpeed
+		self = setmetatable({
 
-				NewSound.Playing = Sound.Playing
-			end
+			_Humanoid = Humanoid,
+			_Player   = Player,
+			_Run	  = Instance.new("Sound"),
+			Jump 	  = Character:GetAttribute("Jump"),
+			Fall	  = Character:GetAttribute("Fall"),
+			Land	 = Character:GetAttribute("Land"),
 
-			Sound.Playing = false
-			Sound = NewSound or Sound
-		end)
+			Connections = {
+				_Running = Humanoid.Running:Connect(function(...)
+					self:Running(...)
+				end),
 
-		Humanoid.Running:Connect(function(Speed)
-			Sound.Playing = ( Speed > 0.1 )
+				_Jumping = Humanoid.Jumping:Connect(function(...)
+					self:Jumping(...)
+				end),
 
-			Sound.PlaybackSpeed = math.clamp(Speed / 9, 0.5, 2)
-		end)
-	end,
-}
+				_Falling = Humanoid.FreeFalling:Connect(function(...)
+					self:FreeFalling(...)
+				end),
+				
+				_FloorMaterial = Humanoid:GetPropertyChangedSignal("FloorMaterial"):Connect(function()
+					self:MaterialChanged(self._Humanoid.FloorMaterial.Name)
+				end)
+			}
+
+		}, Module)
+	end
+	
+	self:MaterialChanged(self._Humanoid.FloorMaterial.Name)
+	
+	return self
+end
+
+
+function Module:Running(Speed)
+	self._Run.Playing = ( Speed > 0.1 )
+end
+
+function Module:Jumping(IsJumping)
+	
+end
+
+function Module:FreeFalling(IsFalling)
+	
+end
+
+function Module:MaterialChanged(Name)
+	local Material   	= StepMap[Name] 
+	local MaterialSound = Material and self._Player:FindFirstChild(Material)
+	local IsPlaying     = self._Run.Playing
+	
+	self._Run.Playing = false
+	
+	if MaterialSound then
+		self._Run = MaterialSound
+		
+		self._Run.Playing = IsPlaying
+	end
+end
+
+function Module:Destroy()
+	for _, Connection in pairs(self.Connections) do
+		Connection:Disconnect()
+	end
+end
+
+return Module
