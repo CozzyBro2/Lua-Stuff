7578cc1a5072f3d638ba6b3dbce0b2c58033433e
GavinW <76234965+CozzyBro2@users.noreply.github.com>
2021-12-18 14:16:32 -0500

    Create init.lua

---
+++ LuaU (Roblox Lua)/Modules/Framework/Client/Main/PlayerJobs/CharacterJobs/init.lua
@@ -0,0 +1,28 @@
+local module = {}
+
+local common = game:GetService("ReplicatedStorage").Modules
+local client = script
+
+local jobs = {
+
+	Local = {
+		
+	},
+
+	Other = {
+		
+	}
+
+}
+
+local performer = require(script.Parent.Parent.JobUtil).new(jobs)
+
+function module.Start(...)
+	performer:PerformJobs("Start", ...)
+end
+
+function module.Stop(...)
+	performer:PerformJobs("Stop", ...)
+end
+
+return module
